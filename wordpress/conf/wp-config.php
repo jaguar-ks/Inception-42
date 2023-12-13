9c346dbec4168d9c9b1339362e64047ee88c2de4
J4GU4R <fahdamineksouss@gmail.com>
2023-12-13 06:13:12 +0100

    WORDPRESS almost done just need the database to start

---
+++ wordpress/conf/wp-config.php
@@ -0,0 +1,27 @@
+<?php
+define('DB_NAME', 'your_database_name');
+define('DB_USER', 'your_database_user');
+define('DB_PASSWORD', 'your_database_password');
+define('DB_HOST', 'mariadb');  // Use the service name of your MariaDB container
+
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+$table_prefix  = 'wp_';  // You can change this to something unique if running multiple installations in the same database
+
+define('WP_DEBUG', false);
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
+
+require_once ABSPATH . 'wp-settings.php';
