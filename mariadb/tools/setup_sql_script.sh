0062eccd416aeb14dab0d5684babffa3bd6def22
jaguar-ks <fahdamineksouss@gmail.com>
2023-12-20 12:32:02 +0100

    fixed the problem of the root user not being able to access the database

--- mariadb/tools/setup_sql_script.sh
+++ mariadb/tools/setup_sql_script.sh
@@ -4,12 +4,14 @@ cat << EOF > init.sql
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PSW}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_R_PSW}';
+FLUSH PRIVILEGES;
 EOF
 
 service mysql start
 
 mysql < init.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
